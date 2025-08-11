5437b8392f929e9a5c54f7d1e05eb1db3f5a9191
hamnapt <hamnapt98@gmail.com>
2025-08-11 16:23:39 +0530

    dashboard changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -99,9 +99,10 @@ function Dashboard() {
               </>  
                )}
             </div>
+            <Outlet/>
           </div>
         </main>
-        <Outlet/>
+        
         <Banner />
 
       </div>
